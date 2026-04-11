95f316e21bbc28e88d5c1c9485424cd4782f0752
JMR_996 <mateusjoseph@gmail.com>
2026-04-10 22:21:38 -0300

    feat: Atv05.cs
    Inserindo enunciado

---
+++ Atv05.cs
@@ -0,0 +1,11 @@
+/*
+Melhore o programa do exercício 4 para que ele possa receber um número inteiro do usuário 
+e mostrar a tabuada desse número. O programa deve continuar pedindo um número até que o 
+usuário digite um número negativo, momento em que o programa deve ser encerrado.
+
+Regras:
+- utilizar do/while em um dos laços de repetição
+- utilizar while em um dos laços de repetição
+- apresentar cada operação de forma organizada
+- mostrar a tabuada completa do número informado pelo usuário
+*/
